957c9d76cd0036cdfea4f89305f3b14c41d5daab
jstar <jstar@localhost.localdomain>
2016-06-30 10:37:42 +0300

    2

--- m631/main.c
+++ m631/main.c
@@ -12,7 +12,15 @@
 key_t key = ftok("/tmp/sem.temp", 1);
 Проинициализируйте их порядковыми номерами [0..15].
 */
-
+    union semun {
+    int val;               /* used for SETVAL only */
+    struct semid_ds *buf;  /* used for IPC_STAT and IPC_SET */
+    ushort *array;         /* used for GETALL and SETALL */
+	};
+    union semun2 {
+    int val;               /* used for SETVAL only */
+    ushort *array;         /* used for GETALL and SETALL */
+	};
 
 int main()
 {
@@ -22,7 +30,7 @@ int main()
     // create
     key_t key = ftok(tmp_file, proj_id);
     if (key <0 ) perror("ftok");
-    int nsems = 16;
+	const int nsems = 16;
     int semflg = IPC_CREAT | IPC_EXCL | 0666;
     
     int semid = semget(key, nsems, semflg);
@@ -34,11 +42,7 @@ int main()
 // initialize 
     int semnum;
     int cmd = SETALL;
-    union semun {
-    int val;               /* used for SETVAL only */
-    struct semid_ds *buf;  /* used for IPC_STAT and IPC_SET */
-    ushort *array;         /* used for GETALL and SETALL */
-} sn;
+    union semun sn;
     sn.array=(ushort*)malloc(sizeof(ushort)*nsems);
     ushort i;
     for (i=0;i<nsems;++i)
@@ -46,7 +50,16 @@ int main()
     int rt = semctl(semid, semnum, cmd, sn);
     if (rt <0 )
         perror("semctl");
-    
+   // read / check
+	   ushort v[nsems]; 
+	union semun2 sn_chck;
+	sn_chck.array = v;
+	cmd = GETALL;
+	int chck = semctl(semid, semnum,cmd,sn_chck);
+	if (chck<0) perror("semctl2");
+	for(i=0; i<nsems;++i)
+		printf("%d\n",v[i]);
+
     return 0;
 }
 
